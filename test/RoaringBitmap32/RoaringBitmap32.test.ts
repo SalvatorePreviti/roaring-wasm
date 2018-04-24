7216f52dd2561534c4828c8c9dae7faa7e268d10
Salvatore Previti <roorback@gmail.com>
2018-04-24 18:20:21 +0100

    addChecked, removeChecked

--- test/RoaringBitmap32/RoaringBitmap32.test.ts
+++ test/RoaringBitmap32/RoaringBitmap32.test.ts
@@ -3,6 +3,38 @@ import RoaringBitmap32 = require('roaring-wasm/RoaringBitmap32')
 import RoaringUint32Array = require('roaring-wasm/RoaringUint32Array')
 
 describe('RoaringBitmap32', () => {
+  describe('addChecked', () => {
+    it('returns false if nothing changes', () => {
+      IDisposable.using(new RoaringBitmap32([123]), bitmap => {
+        expect(bitmap.addChecked(123)).toBe(false)
+        expect(bitmap.cardinality()).toBe(1)
+      })
+    })
+
+    it('returns true if something changes', () => {
+      IDisposable.using(new RoaringBitmap32([124]), bitmap => {
+        expect(bitmap.addChecked(123)).toBe(true)
+        expect(bitmap.cardinality()).toBe(2)
+      })
+    })
+  })
+
+  describe('removeChecked', () => {
+    it('returns false if nothing changes', () => {
+      IDisposable.using(new RoaringBitmap32([125]), bitmap => {
+        expect(bitmap.removeChecked(123)).toBe(false)
+        expect(bitmap.cardinality()).toBe(1)
+      })
+    })
+
+    it('returns true if something changes', () => {
+      IDisposable.using(new RoaringBitmap32([123]), bitmap => {
+        expect(bitmap.removeChecked(123)).toBe(true)
+        expect(bitmap.cardinality()).toBe(0)
+      })
+    })
+  })
+
   describe('from array', () => {
     const array = [123, 189, 456, 789, 910]
 
