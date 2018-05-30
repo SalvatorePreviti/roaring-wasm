1dd84f06e58c42ba14e4793145e5e0a9e6701192
Salvatore Previti <roorback@gmail.com>
2018-05-30 13:23:13 +0100

    more-tests

--- test/unit/RoaringUint8Array.test.ts
+++ test/unit/RoaringUint8Array.test.ts
@@ -46,4 +46,87 @@ describe('RoaringUint8Array', () => {
       expect(p.isDisposed).toBe(false)
     })
   })
+
+  describe('asTypedArray', () => {
+    it('returns a valid view', () => {
+      IDisposable.using(new RoaringUint8Array([1, 2, 3]), p => {
+        const buf = p.asTypedArray()
+        expect(buf.length).toBe(3)
+        expect(buf[0]).toBe(1)
+        expect(buf[1]).toBe(2)
+        expect(buf[2]).toBe(3)
+        expect(buf.buffer === p.buffer).toBeTruthy()
+      })
+    })
+  })
+
+  describe('toTypedArray', () => {
+    it('returns a copy', () => {
+      IDisposable.using(new RoaringUint8Array([1, 2, 3]), p => {
+        const buf = p.toTypedArray()
+        expect(buf.length).toBe(3)
+        expect(buf[0]).toBe(1)
+        expect(buf[1]).toBe(2)
+        expect(buf[2]).toBe(3)
+        expect(buf.buffer !== p.buffer).toBeTruthy()
+      })
+    })
+  })
+
+  describe('asNodeBuffer', () => {
+    it('returns a valid view', () => {
+      IDisposable.using(new RoaringUint8Array([1, 2, 3]), p => {
+        const buf = p.asNodeBuffer()
+        expect(buf.length).toBe(3)
+        expect(buf[0]).toBe(1)
+        expect(buf[1]).toBe(2)
+        expect(buf[2]).toBe(3)
+        expect(buf.buffer === p.buffer).toBeTruthy()
+      })
+    })
+  })
+
+  describe('toNodeBuffer', () => {
+    it('returns a copy', () => {
+      IDisposable.using(new RoaringUint8Array([1, 2, 3]), p => {
+        const buf = p.toNodeBuffer()
+        expect(buf.length).toBe(3)
+        expect(buf[0]).toBe(1)
+        expect(buf[1]).toBe(2)
+        expect(buf[2]).toBe(3)
+        expect(buf.buffer !== p.buffer).toBeTruthy()
+      })
+    })
+  })
+
+  describe('dispose', () => {
+    it('can be called twice', () => {
+      const p = new RoaringUint8Array([1, 2, 3])
+      expect(p.dispose()).toBe(true)
+      expect(p.isDisposed).toBe(true)
+      expect(p.dispose()).toBe(false)
+      expect(p.isDisposed).toBe(true)
+    })
+
+    it('resets everything', () => {
+      const p = new RoaringUint8Array([1, 2, 3])
+      expect(p.dispose()).toBe(true)
+      expect(p.byteLength).toBe(0)
+      expect(p.byteOffset).toBe(0)
+    })
+  })
+
+  describe('throwIfDisposed', () => {
+    it('does not throw if not disposed', () => {
+      IDisposable.using(new RoaringUint8Array(5), p => {
+        p.throwIfDisposed()
+      })
+    })
+
+    it('throws if disposed', () => {
+      const t = new RoaringUint8Array(4)
+      t.dispose()
+      expect(() => t.throwIfDisposed()).toThrow()
+    })
+  })
 })
