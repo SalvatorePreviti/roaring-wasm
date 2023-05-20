6b6e8f38600e6a16a1f54ac0864bbad19c252ab0
Salvatore Previti <roorback@gmail.com>
2023-05-20 17:47:09 +0100

    fixes

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-const { ROOT_FOLDER } = require("./config/paths");
+const { ROOT_FOLDER, ROARING_WASM_SRC_WASM_MODULE_OUT_FOLDER } = require("./config/paths");
 const { runMain, forkAsync } = require("./lib/utils");
+const fs = require("fs");
+const path = require("path");
 
 if (require.main === module) {
   process.chdir(ROOT_FOLDER);
@@ -9,6 +11,10 @@ if (require.main === module) {
   require("ts-node").register();
 
   runMain(() => {
+    if (!fs.existsSync(path.resolve(ROARING_WASM_SRC_WASM_MODULE_OUT_FOLDER, "index.wasm"))) {
+      throw new Error("Please run `npm run build` before running tests");
+    }
+
     process.argv.push("--recursive");
     process.argv.push("test/unit/**/*.test.ts");
 
