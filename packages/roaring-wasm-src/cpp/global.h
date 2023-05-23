3d144ced0d4e36a8f23d1fb27c4ba75d80a6d6e2
Salvatore Previti <roorback@gmail.com>
2023-05-23 11:51:28 +0100

    update roaring, fix lint

--- packages/roaring-wasm-src/cpp/global.h
+++ packages/roaring-wasm-src/cpp/global.h
@@ -5,10 +5,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CROARING_SILENT_BUILD
+
 // Optimization - disable IO and assertions
 
-// #define printf(...) (0)
-// #define fprintf(...) (0)
-// #define assert(...)
+#define printf(...) (0)
+#define fprintf(...) (0)
+#define assert(...)
 
 #endif
